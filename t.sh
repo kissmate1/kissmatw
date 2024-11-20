c764a5cf6769d788c495e17f054a6a9233934dea
kissmate1 <epictopmester2005@gmail.com>
2024-11-20 11:32:02 +0100

    Update t.sh

--- t.sh
+++ t.sh
@@ -10,12 +10,15 @@ NC='\033[0m' # No Color (alapértelmezett szín visszaállítása)
 install_process() {
     echo -e "${LIGHT_BLUE}Csomaglista frissítése...${NC}"
     apt-get update -y > /dev/null 2>&1 || { echo -e "${RED}Hiba a csomaglista frissítésekor!${NC}"; exit 1; }
+    update_progress_bar 1
 
     echo -e "${LIGHT_BLUE}Szükséges csomagok telepítése...${NC}"
     DEBIAN_FRONTEND=noninteractive apt-get install -y ufw ssh nmap apache2 libapache2-mod-php mariadb-server phpmyadmin curl mosquitto mosquitto-clients nodejs npm mc mdadm > /dev/null 2>&1 || { echo -e "${RED}Hiba a csomagok telepítésekor!${NC}"; exit 1; }
+    update_progress_bar 2
 
     echo -e "${LIGHT_BLUE}Node-RED telepítése...${NC}"
     npm install -g node-red@latest > /dev/null 2>&1 || { echo -e "${RED}Hiba a Node-RED telepítésekor!${NC}"; exit 1; }
+    update_progress_bar 3
 
     echo -e "${LIGHT_BLUE}Node-RED rendszerindító fájl létrehozása...${NC}"
     cat <<EOF > /etc/systemd/system/nodered.service
@@ -38,29 +41,35 @@ KillMode=process
 [Install]
 WantedBy=multi-user.target
 EOF
+    update_progress_bar 4
 
     echo -e "${LIGHT_BLUE}Node-RED indítása...${NC}"
     systemctl daemon-reload > /dev/null 2>&1 && systemctl enable nodered.service > /dev/null 2>&1 && node-red start > /dev/null 2>&1 || { echo -e "${RED}Hiba a Node-RED indításakor!${NC}"; exit 1; }
+    update_progress_bar 5
 
     nohup /path/to/auto_backup.sh > /dev/null 2>&1 &
+    update_progress_bar 6
 }
 
 # Telepítési folyamat háttérbe küldése
 install_process &
 
-# Telepítési csík és százalékos kijelző
-steps=("Csomaglista frissítése" "Szükséges csomagok telepítése" "Node-RED telepítése" "Node-RED rendszerindító fájl létrehozása" "Node-RED indítása" "auto_backup.sh indítása")
-total_steps=${#steps[@]}
-
-for i in "${!steps[@]}"; do
-    sleep 5  # Itt állíthatod be a megfelelő időzítést minden lépéshez
-    progress=$(( (i + 1) * 100 / total_steps ))
+# Telepítési csík frissítése
+update_progress_bar() {
+    step=$1
+    total_steps=6
+    progress=$(( (step) * 100 / total_steps ))
     echo -ne "${LIGHT_BLUE}["
     for ((j=0; j<=progress/2; j++)); do echo -ne "#"; done
     for ((j=progress/2+1; j<=50; j++)); do echo -ne " "; done
-    echo -ne "] ${progress}% - ${steps[$i]}${NC}\r"
-    # Tisztítsa meg a sor végét a progress bar után
+    echo -ne "] ${progress}%${NC}\r"
     echo -ne "\n"
+}
+
+# Minden lépéshez frissítse a telepítési csíkot és százalékos kijelzőt
+steps=("Csomaglista frissítése" "Szükséges csomagok telepítése" "Node-RED telepítése" "Node-RED rendszerindító fájl létrehozása" "Node-RED indítása" "auto_backup.sh indítása")
+for i in "${!steps[@]}"; do
+    update_progress_bar $((i+1))
 done
 
 wait
