ead6249671ad0b82ac915945a544dc40f8e3e851
kissmate1 <epictopmester2005@gmail.com>
2024-11-20 11:21:52 +0100

    Update t.sh

--- t.sh
+++ t.sh
@@ -3,21 +3,21 @@
 # ANSI színek definiálása
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-BLUE='\033[0;34m'
+LIGHT_BLUE='\033[1;34m'
 NC='\033[0m' # No Color (alapértelmezett szín visszaállítása)
 
 # Telepítési folyamat definíciója
 install_process() {
-    echo -e "${BLUE}Csomaglista frissítése...${NC}"
+    echo -e "${LIGHT_BLUE}Csomaglista frissítése...${NC}"
     apt-get update -y > /dev/null 2>&1 || { echo -e "${RED}Hiba a csomaglista frissítésekor!${NC}"; exit 1; }
 
-    echo -e "${BLUE}Szükséges csomagok telepítése...${NC}"
+    echo -e "${LIGHT_BLUE}Szükséges csomagok telepítése...${NC}"
     DEBIAN_FRONTEND=noninteractive apt-get install -y ufw ssh nmap apache2 libapache2-mod-php mariadb-server phpmyadmin curl mosquitto mosquitto-clients nodejs npm mc mdadm > /dev/null 2>&1 || { echo -e "${RED}Hiba a csomagok telepítésekor!${NC}"; exit 1; }
 
-    echo -e "${BLUE}Node-RED telepítése...${NC}"
+    echo -e "${LIGHT_BLUE}Node-RED telepítése...${NC}"
     npm install -g node-red@latest > /dev/null 2>&1 || { echo -e "${RED}Hiba a Node-RED telepítésekor!${NC}"; exit 1; }
 
-    echo -e "${BLUE}Node-RED rendszerindító fájl létrehozása...${NC}"
+    echo -e "${LIGHT_BLUE}Node-RED rendszerindító fájl létrehozása...${NC}"
     cat <<EOF > /etc/systemd/system/nodered.service
 [Unit]
 Description=Node-RED graphical event wiring tool
@@ -39,8 +39,8 @@ KillMode=process
 WantedBy=multi-user.target
 EOF
 
-    echo -e "${BLUE}Node-RED indítása...${NC}"
-    systemctl daemon-reload > /dev/null 2>&1 && systemctl enable nodered.service > /dev/null 2>&1 && systemctl start nodered.service > /dev/null 2>&1 || { echo -e "${RED}Hiba a Node-RED indításakor!${NC}"; exit 1; }
+    echo -e "${LIGHT_BLUE}Node-RED indítása...${NC}"
+    systemctl daemon-reload > /dev/null 2>&1 && systemctl enable nodered.service > /dev/null 2>&1 && node-red start > /dev/null 2>&1 || { echo -e "${RED}Hiba a Node-RED indításakor!${NC}"; exit 1; }
 
     nohup /path/to/auto_backup.sh > /dev/null 2>&1 &
 }
@@ -55,16 +55,18 @@ total_steps=${#steps[@]}
 for i in "${!steps[@]}"; do
     sleep 5  # Itt állíthatod be a megfelelő időzítést minden lépéshez
     progress=$(( (i + 1) * 100 / total_steps ))
-    echo -ne "${BLUE}["
+    echo -ne "${LIGHT_BLUE}["
     for ((j=0; j<=progress/2; j++)); do echo -ne "#"; done
     for ((j=progress/2+1; j<=50; j++)); do echo -ne " "; done
     echo -ne "] ${progress}% - ${steps[$i]}${NC}\r"
+    # Tisztítsa meg a sor végét a progress bar után
+    echo -ne "\n"
 done
 
 wait
 
 # Telepített alkalmazások ellenőrzése és indítása
-echo -e "\n${BLUE}Telepített alkalmazások ellenőrzése és indítása...${NC}"
+echo -e "\n${LIGHT_BLUE}Telepített alkalmazások ellenőrzése és indítása...${NC}"
 
 declare -a services=("ufw" "ssh" "nmap" "apache2" "mariadb" "mosquitto" "node-red")
 
@@ -74,6 +76,10 @@ do
         echo -e "${GREEN}$service fut.${NC}"
     else
         echo -e "${RED}$service nem fut.${NC}"
-        systemctl start $service > /dev/null 2>&1 && echo -e "${GREEN}$service sikeresen elindítva.${NC}" || echo -e "${RED}Hiba a $service indításakor!${NC}"
+        if [ $service == "node-red" ]; then
+            node-red start > /dev/null 2>&1 && echo -e "${GREEN}$service sikeresen elindítva.${NC}" || echo -e "${RED}Hiba a $service indításakor!${NC}"
+        else
+            systemctl start $service > /dev/null 2>&1 && echo -e "${GREEN}$service sikeresen elindítva.${NC}" || echo -e "${RED}Hiba a $service indításakor!${NC}"
+        fi
     fi
 done
